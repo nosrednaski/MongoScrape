5b1bd1f8bfbab81d778fe06e5ff4a8379f63c1f6
Unknown <nosrednaski@gmail.com>
2018-09-20 17:34:16 -0400

    Added some functionality
    -delete comments
    -scrape scores
    -dates for when articles and notes are created

--- models/Article.js
+++ models/Article.js
@@ -1,11 +1,12 @@
-var mongoose = require("mongoose");
-
+const mongoose = require("mongoose");
+const moment = require("moment");
 // Save a reference to the Schema constructor
-var Schema = mongoose.Schema;
+const Schema = mongoose.Schema;
 
+let articleDate = moment().format("MMM D YYYY, kk:mm")
 // Using the Schema constructor, create a new UserSchema object
 // This is similar to a Sequelize model
-var ArticleSchema = new Schema({
+const ArticleSchema = new Schema({
   // `title` is required and of type String
   title: {
     type: String,
@@ -16,17 +17,25 @@ var ArticleSchema = new Schema({
     type: String,
     required: true
   },
+  score: {
+    type: String,
+    required: true
+  },
   // `note` is an object that stores a Note id
   // The ref property links the ObjectId to the Note model
   // This allows us to populate the Article with an associated Note
   note: {
     type: Schema.Types.ObjectId,
     ref: "Note"
+  },
+  dateCreated: {
+    type: String,
+    default: articleDate
   }
 });
 
 // This creates our model from the above schema, using mongoose's model method
-var Article = mongoose.model("Article", ArticleSchema);
+const Article = mongoose.model("Article", ArticleSchema);
 
 // Export the Article model
 module.exports = Article;
--- server.js
+++ server.js
@@ -12,7 +12,7 @@ var cheerio = require("cheerio");
 // Require all models
 var db = require("./models");
 
-var PORT = 3000;
+var PORT = 8000;
 
 // Initialize Express
 var app = express();
@@ -27,60 +27,51 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 // Connect to the Mongo DB
-mongoose.connect("mongodb://localhost/week18Populater", { useNewUrlParser: true });
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
 
-// Routes
+mongoose.Promise = Promise;
+mongoose.connect(MONGODB_URI);
 
-// A GET route for scraping the echoJS website
 app.get("/scrape", function(req, res) {
-  // First, we grab the body of the html with request
-  axios.get("http://www.echojs.com/").then(function(response) {
-    // Then, we load that into cheerio and save it to $ for a shorthand selector
+  axios.get("https://news.ycombinator.com/").then(function(response) {
     var $ = cheerio.load(response.data);
 
-    // Now, we grab every h2 within an article tag, and do the following:
-    $("article h2").each(function(i, element) {
-      // Save an empty result object
+    $(".athing").each(function(i, element) {
       var result = {};
 
-      // Add the text and href of every link, and save them as properties of the result object
-      result.title = $(this)
-        .children("a")
-        .text();
-      result.link = $(this)
-        .children("a")
-        .attr("href");
+      result.title = $(element).children('.title').children('a').text();
+      result.link = $(element).children('.title').children('a').attr('href');
+      result.score = $(element).next('tr').children('.subtext').children('.score').text();
 
-      // Create a new Article using the `result` object built from scraping
       db.Article.create(result)
         .then(function(dbArticle) {
-          // View the added result in the console
           console.log(dbArticle);
         })
         .catch(function(err) {
-          // If an error occurred, send it to the client
           return res.json(err);
         });
     });
-
-    // If we were able to successfully scrape and save an Article, send a message to the client
     res.send("Scrape Complete");
   });
 });
 
 // Route for getting all Articles from the db
-app.get("/articles", function(req, res) {
+
+  app.get("/articles", function(req, res) {
   // Grab every document in the Articles collection
   db.Article.find({})
     .then(function(dbArticle) {
       // If we were able to successfully find Articles, send them back to the client
       res.json(dbArticle);
+      return dbArticle
     })
     .catch(function(err) {
       // If an error occurred, send it to the client
       res.json(err);
     });
 });
+
+
 
 // Route for grabbing a specific Article by id, populate it with it's note
 app.get("/articles/:id", function(req, res) {
@@ -116,6 +107,27 @@ app.post("/articles/:id", function(req, res) {
       // If an error occurred, send it to the client
       res.json(err);
     });
+});
+
+app.delete("/notes/:id", function(req, res) {
+  db.Note.findByIdAndDelete(req.params.id)
+    .then(note => {
+    if(!note) {
+        return res.status(404).send({
+            message: "Note not found with id " + req.params.noteId
+        });
+    }
+    res.send({message: "Note deleted successfully!"});
+  }).catch(err => {
+    if(err.kind === 'ObjectId' || err.name === 'NotFound') {
+        return res.status(404).send({
+            message: "Note not found with id " + req.params.noteId
+        });                
+    }
+    return res.status(500).send({
+        message: "Could not delete note with id " + req.params.noteId
+    });
+  });
 });
 
 // Start the server
